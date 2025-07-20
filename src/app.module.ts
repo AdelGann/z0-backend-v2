4abe6f5fe2c2af06ff463fcbc574b58e8b3eadae
AdelGann <imadelg14@gmail.com>
2025-07-19 22:53:37 -0400

    feat(core): add config and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './common/db/db.module';
 import { AuthModule } from './auth/auth.module';
+import { configModule } from './config/config.service';
 
 @Module({
-  imports: [UsersModule, DbModule, AuthModule,],
+  imports: [configModule, UsersModule, DbModule, AuthModule],
 })
-export class AppModule { }
+export class AppModule {}
