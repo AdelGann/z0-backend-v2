453b80f312219385553a737c349294b6c40dfca8
AdelGann <imadelg14@gmail.com>
2025-07-31 19:34:36 -0400

    feat(employees): add employees services

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { employees } from 'generated/prisma';
 import { DbService } from 'src/common/db/db.service';
 
 @Injectable()
 export class EmployeesService {
   constructor(private readonly dbService: DbService) {}
 
-  async getAll() {}
-  async create(user_id: string, founder_id: string) {}
+  async getAll(org_id: string) {
+    return this.dbService.employees.findMany({
+      where: {
+        org_id,
+      },
+    });
+  }
+
+  //TODO: Agregar decorador para validar si el usuario que está realizando la operacion tiene permisos
+  async create(user_id: string, org_id: string): Promise<employees> {
+    return this.dbService.employees.create({
+      data: {
+        user_id: user_id,
+        org_id: org_id,
+      },
+    });
+  }
   async createMany() {}
-  async remove() {}
+
+  //TODO: Agregar decorador para validar si el usuario que está realizando la operacion tiene permisos
+  async update(user_id: string, org_id: string) {}
+
+  async remove(org_id: string, user_id: string): Promise<employees> {
+    return this.dbService.employees.delete({
+      where: {
+        user_id,
+        org_id,
+      },
+    });
+  }
 }
