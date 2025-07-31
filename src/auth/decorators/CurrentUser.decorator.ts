c0d6600dd0c24cbc25335586064189588b174dbc
AdelGann <imadelg14@gmail.com>
2025-07-31 19:58:02 -0400

    feat(auth): fix decorators typing, roles.metadata file moved

--- src/auth/decorators/CurrentUser.decorator.ts
+++ src/auth/decorators/CurrentUser.decorator.ts
@@ -1,8 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { users } from 'generated/prisma';
+import { token_payload } from '../interfaces/Token_Payload.interface';
+import { Request } from 'express';
+
+interface request extends Request {
+  user: users | token_payload;
+}
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request: request = ctx.switchToHttp().getRequest();
     return request.user;
   },
 );
--- src/auth/decorators/index.ts
+++ src/auth/decorators/index.ts
@@ -1,4 +1,3 @@
-export { RequireRoles, ROLES_KEY } from './RequireRoles.decorador';
 export { CurrentUser } from './CurrentUser.decorator';
 export { Auth } from './Auth.decorator';
 export { AuthRoles } from './AuthRoles.decorator';
