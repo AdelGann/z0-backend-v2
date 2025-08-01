59eb65107372170e506dcbc71b2d486230735405
AdelGann <imadelg14@gmail.com>
2025-07-31 23:13:59 -0400

    feat(org): add create employee function

--- src/org/org.service.ts
+++ src/org/org.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { DbService } from 'src/common/db/db.service';
+import { EmployeesService } from 'src/employees/employees.service';
 import { CreateOrgDto, UpdateOrgDto } from './dto/org.dto';
 
 @Injectable()
@@ -12,29 +13,32 @@ export class OrgService {
   constructor(
     private readonly dbService: DbService,
     private readonly usersService: UsersService,
+    private readonly employeesService: EmployeesService,
   ) {}
 
-  // Solo para apartado administrador
+  //NOTE: Solo para apartado administrador
   async getAll() {
     return this.dbService.orgs.findMany();
   }
 
+  //NOTE: 30/07
   // Servicio solo valido para usuarios miembros de una organización, NO externos
   // ¿Es necesario validar que el usuario sea miembro de la organización?
   // ¿Tan siquiera es necesario un get por id?
+
+  //NOTE: 31/07: no importa, como sea, al usar el guardian ya no hace falta hacer alguna validación extra
   async getById(id: string) {
     return this.dbService.orgs.findUnique({
       where: { id },
     });
   }
 
-  // Reutilizar servicio para la creación de usuarios
   async create(org: CreateOrgDto, founder_id: string) {
     const user = await this.usersService.getById(founder_id);
     if (user === null) {
       throw new NotFoundException('User not founded');
     }
-    return this.dbService.orgs.create({
+    const new_org = await this.dbService.orgs.create({
       data: {
         name: org.name,
         founder_id: user.id,
@@ -42,9 +46,14 @@ export class OrgService {
         update_at: new Date(),
       },
     });
+    await this.employeesService.create(user.id, {
+      org_id: new_org.id,
+      role: 'ADMIN',
+    });
+    return new_org;
   }
 
-  // SOLO administradores de la organización
+  // NOTE: SOLO administradores de la organización
   // Debería agregar el ID de la organización en el DTO?
   async update(updateOrg: UpdateOrgDto, founder_id: string) {
     const user = await this.usersService.getById(founder_id);
@@ -57,6 +66,7 @@ export class OrgService {
     if (org === null) {
       throw new NotFoundException('Organization not founded');
     }
+
     //TODO: falta validar usuarios empleados que tengan permisos para modificar
     //?: Aunque no es necesario que más de una persona pueda modificar la organización.
     //Evaluar
@@ -74,7 +84,7 @@ export class OrgService {
     });
   }
 
-  // Servicio debería inactivar la organización, de momento se está trabajando con eliminado fisico
+  //NOTE: Servicio debería inactivar la organización, de momento se está trabajando con eliminado fisico
   // Realizar despues de terminar de cambiar el prisma.
   // TODO: CAMBIAR eliminado fisico a lógico.
   async delete() {}
