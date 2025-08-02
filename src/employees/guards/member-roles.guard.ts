0fb6babb4536c75d383de9035ee245253c3c8856
AdelGann <imadelg14@gmail.com>
2025-08-01 20:23:35 -0400

    feat(member): fix member guards

--- src/employees/guards/member-roles.guard.ts
+++ src/employees/guards/member-roles.guard.ts
@@ -1,41 +1,54 @@
 import {
   Injectable,
-  CanActivate,
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 import { DbService } from 'src/common/db/db.service';
 import { Roles } from 'generated/prisma';
 
 @Injectable()
-export class MemberRolesGuard implements CanActivate {
+export class MemberRolesGuard extends AuthGuard('jwt') {
   constructor(
     private reflector: Reflector,
     private dbService: DbService,
-  ) {}
+  ) {
+    super();
+  }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const can = await super.canActivate(context);
+    if (!can) {
+      return false;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<Roles[]>('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
+
     if (!requiredRoles || requiredRoles.length === 0) {
       return true;
     }
 
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    const orgId = request.params.organizationId || request.body.organizationId;
 
-    if (!user || !orgId) {
+    const org_id =
+      request?.params?.org_id ||
+      request?.body?.org_id ||
+      request?.query?.org_id ||
+      null;
+
+    if (!user || !org_id) {
       throw new ForbiddenException('Invalid User or Organization');
     }
 
     const employee = await this.dbService.employees.findFirst({
       where: {
         user_id: user.id,
-        org_id: orgId,
+        org_id,
         role: { in: requiredRoles },
       },
     });
--- src/employees/guards/member.guard.ts
+++ src/employees/guards/member.guard.ts
@@ -1,19 +1,30 @@
 import {
   Injectable,
-  CanActivate,
   ExecutionContext,
   ForbiddenException,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { DbService } from 'src/common/db/db.service';
 
 @Injectable()
-export class MemberGuard implements CanActivate {
-  constructor(private dbService: DbService) {}
+export class MemberGuard extends AuthGuard('jwt') {
+  constructor(private dbService: DbService) {
+    super();
+  }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const can = await super.canActivate(context);
+    if (!can) {
+      return false;
+    }
+
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    const org_id = request.params.org_id || request.body.org_id;
+    const org_id =
+      request?.params?.org_id ||
+      request?.body?.org_id ||
+      request?.query?.org_id ||
+      null;
 
     if (!user || !org_id) {
       throw new ForbiddenException('Invalid User or Organization');
