c442f329c102c6957e9208b5b3bd6cb1ae9b40e2
AdelGann <imadelg14@gmail.com>
2025-08-07 15:44:19 -0400

    feat(debts): add debts dto

---
+++ src/debts/dto/debts.dto.ts
@@ -0,0 +1,89 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+} from 'class-validator';
+import { Money, PaymentStatus, PaymentType } from 'generated/prisma';
+
+export class CreateDebtDto {
+  @ApiProperty({
+    description: 'Debt type name',
+    type: String,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  debt_type!: string;
+
+  @ApiProperty({
+    description: 'Payment type',
+    enum: PaymentType,
+    required: true,
+    default: PaymentType.credit,
+    example: PaymentType.credit,
+  })
+  @IsNotEmpty()
+  @IsEnum(PaymentType)
+  payment_type!: PaymentType;
+
+  @ApiProperty({
+    description: 'Money type',
+    enum: Money,
+    required: true,
+    default: Money.Bs,
+    example: Money.Bs,
+  })
+  @IsNotEmpty()
+  @IsEnum(Money)
+  money_type!: Money;
+
+  @ApiProperty({
+    description: 'Amount',
+    default: 0,
+    required: true,
+    example: 100,
+    type: Number,
+    minimum: 0,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  amount!: number;
+
+  @ApiProperty({
+    description: 'Employee id',
+    type: String,
+    required: false,
+    example: '1512-1258-1258',
+  })
+  @IsUUID()
+  @IsOptional()
+  employee_id?: string;
+}
+
+export class UpdateStatusDebtDtop {
+  @ApiProperty({
+    description: 'debt id',
+    required: true,
+    example: '1512-1258-1258',
+    type: String,
+  })
+  @IsNotEmpty()
+  @IsUUID()
+  id!: string;
+
+  @ApiProperty({
+    description: 'Payment status',
+    default: PaymentStatus.PAID,
+    enum: PaymentStatus,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsEnum(PaymentStatus)
+  status!: PaymentStatus;
+}
